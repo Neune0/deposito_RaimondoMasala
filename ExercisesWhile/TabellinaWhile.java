e8bd9cfd5888604515118251a8d2c96a9b28564c
Raimon <60296186+Neune0@users.noreply.github.com>
2025-02-19 15:22:16 +0100

    esercizio tabellina con while

---
+++ ExercisesWhile/TabellinaWhile.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TabellinaWhile {
+    public static void main(String[] args) {
+        // crezione scanner
+        Scanner myScanner = new Scanner(System.in);
+
+        // richiesta e inserimento dell'input utente
+        System.out.println("Inserisci il numero del quale vuoi visualizzare la tabellina(fino a 10): ");
+        int numeroScelto = myScanner.nextInt();
+
+        int iterator = 1; // iteratore per creazione tabellina
+        System.out.println("Tabellina del "+numeroScelto+"\n");
+
+        while(iterator<10){
+            System.out.println("| "+numeroScelto+" x "+iterator+" : "+iterator*numeroScelto);
+            iterator++;
+        }
+    }
+}
