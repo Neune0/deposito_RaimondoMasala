d66a393b06be31023550202838b708080d69873e
Raimon <60296186+Neune0@users.noreply.github.com>
2025-02-19 17:31:41 +0100

    primo esempio di foreach

---
+++ EsempiForEach/EsempioForEach.java
@@ -0,0 +1,10 @@
+package EsempiForEach;
+public class EsempioForEach {
+    public static void main(String[] args) {
+        int[] numeri = {1,2,3,4,5,6,7};
+
+        for(int numero : numeri){
+            System.out.println(numero);
+        }
+    }
+}
