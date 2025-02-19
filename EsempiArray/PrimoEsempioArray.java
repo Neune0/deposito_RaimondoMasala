ccb2e698984be0acba3df10eee9aec74fe76e243
Raimon <60296186+Neune0@users.noreply.github.com>
2025-02-19 17:40:03 +0100

    primo esempio con array

---
+++ EsempiArray/PrimoEsempioArray.java
@@ -0,0 +1,10 @@
+public class PrimoEsempioArray {
+    public static void main(String[] args) {
+        int[] numeri = {1,2,3,4,5,6};
+
+        for(int i=0;i<numeri.length;i++){
+            System.out.println(numeri[i]);
+        }
+    }
+    
+}
