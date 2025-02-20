c375f74c31fe80c684aa1da09e5ae4079e589e14
Raimon <60296186+Neune0@users.noreply.github.com>
2025-02-20 11:56:39 +0100

    primo esempio con arraylist

--- EsempiArrayList/EsempioArrayList.java
+++ EsempiArrayList/EsempioArrayList.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class EsempioArrayList {
     public static void main(String[] args) {
@@ -10,5 +11,20 @@ public class EsempioArrayList {
         primoArrayList.set(0, "ciao modificato");
         // stampo il primo elemento dell'array
         System.out.println("il primo elemento dell'array"+primoArrayList.get(0));
+
+        // size
+        System.out.println("la dimensione dell'array è: "+primoArrayList.size());
+
+        // remove
+        primoArrayList.remove(0);
+
+        // clear rimuove tutti gli elementi
+        primoArrayList.clear();
+
+        // from array to array list
+        String[] arrayStringhe = {"ciao","ciao"};
+
+        ArrayList<String> secondoArrayList = new ArrayList<>(Arrays.asList(arrayStringhe));
+
     }
 }
