bac6d8ecc1ead1e03dbe9bdaded1aa7f2f4f4fbc
Raimon <60296186+Neune0@users.noreply.github.com>
2025-03-04 16:57:44 +0100

    forwarding exception primo esempio

--- EsempiException/PrimoEsempioException.java
+++ EsempiException/PrimoEsempioException.java
@@ -1,11 +1,12 @@
 
 public class PrimoEsempioException {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try{
             int[] numeri = new int[5];
             numeri[10] = 10;
         } catch (ArrayIndexOutOfBoundsException e) {    
             System.out.println("Indice fuori dai limiti");
+            throw new Exception(e);
         }finally{
             System.out.println("il try catch è finito");
         }
