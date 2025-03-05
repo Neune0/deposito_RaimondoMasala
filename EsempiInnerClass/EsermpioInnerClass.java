4b46a66e57d5e80bb2ea65a1e93b91e296b5fac1
Raimon <60296186+Neune0@users.noreply.github.com>
2025-03-05 17:30:44 +0100

    secondo esempio con inner class

--- EsempiInnerClass/EsermpioInnerClass.java
+++ EsempiInnerClass/EsermpioInnerClass.java
@@ -4,12 +4,24 @@ public class EsermpioInnerClass{
         ClasseFuori.ClasseDentro cd = cf.new ClasseDentro();
         System.out.println("x: "+cf.x);
         System.out.println("y: "+cd.y);
+
+        /*----------------------------------------*/
+
+        OuterClass.InnerClass myInner = new OuterClass.InnerClass();
+        System.out.println(myInner.y);
     }
 }
 
 class ClasseFuori{
     int x= 10;
     class ClasseDentro{
+        int y = 20;
+    }
+}
+
+class OuterClass{
+    int x = 10;
+    static class InnerClass{
         int y = 20;
     }
 }
