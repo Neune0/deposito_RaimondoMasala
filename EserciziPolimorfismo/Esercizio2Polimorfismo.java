8d48b1015e6349e897bce780af7bb8e0ab207c3a
Raimon <60296186+Neune0@users.noreply.github.com>
2025-03-06 10:21:44 +0100

    piccola modifica

--- EserciziPolimorfismo/Esercizio2Polimorfismo.java
+++ EserciziPolimorfismo/Esercizio2Polimorfismo.java
@@ -4,7 +4,7 @@ public class Esercizio2Polimorfismo {
     public static void main(String[] args) {
         Persona persona1 = new Persona();
         Persona persona2 = new Persona();
-        Persona pirata = new Pirata();
+        Pirata pirata = new Pirata();
         persona1.saluta();
         persona2.saluta();
         pirata.saluta();
